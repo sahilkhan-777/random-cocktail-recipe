9e519e5bc58ed52e654e4696e03087594b4092a9
Sahil Khan <78162192+sahilkhan-777@users.noreply.github.com>
2025-11-10 22:07:33 +0530

    Initial functional version with basic styling and core features working

---
+++ index.js
@@ -0,0 +1,27 @@
+import express from "express";
+import axios from "axios";
+
+const app = express();
+const port = 3000;
+
+app.use(express.static("public"));
+app.set("view engine", "ejs");
+
+app.get("/", async (req, res) => {
+    try {
+        const response = await axios.get("https://www.thecocktaildb.com/api/json/v1/1/random.php");
+        const result = response.data;
+
+        // console.log(result.drinks[0]);
+        // console.log(result.drinks[0].strDrink);
+
+        res.render("index.ejs", { recipe : result.drinks[0] });
+    }
+    catch(error){
+        console.error(`Error: ${error.message}`);
+    }
+});
+
+app.listen(port, () => {
+    console.log(`Server listening on port: ${port}`);
+});
