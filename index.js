e5edfe8f017581461de0c643eb04e5ca8d3656b0
Sahil Khan <78162192+sahilkhan-777@users.noreply.github.com>
2025-11-14 23:40:33 +0530

    Add new routes and remove browse route

--- index.js
+++ index.js
@@ -16,10 +16,61 @@ app.get("/random", async (req, res) => {
         const response = await axios.get("https://www.thecocktaildb.com/api/json/v1/1/random.php");
         const result = response.data;
 
-        // console.log(result.drinks[0]);
-        // console.log(result.drinks[0].strDrink);
+        res.render("random.ejs", { recipe: result.drinks[0] });
+    }
+    catch (error) {
+        console.error(`Error: ${error.message}`);
+    }
+});
 
-        res.render("random.ejs", { recipe : result.drinks[0] });
+//search endpoint
+app.get("/search", async (req, res) => {
+    const name = req.query.name;
+    const searchURL = `https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${encodeURIComponent(name)}`;
+
+    try {
+        const response = await axios.get(searchURL);
+        const result = response.data.drinks;
+
+        res.render("search.ejs", { recipe: result });
+    }
+    catch(error){
+        console.error(`Error: ${error.message}`);
+    }
+});
+
+
+// featured recipes
+app.get("/alabama", async (req, res) => {
+    try {
+        const response = await axios.get("https://www.thecocktaildb.com/api/json/v1/1/search.php?s=alabama");
+        const result = response.data.drinks;
+
+        res.render("featured.ejs", { recipe: result });
+    }
+    catch(error){
+        console.error(`Error: ${error.message}`);
+    }
+});
+
+app.get("/autodafe", async (req, res) => {
+    try {
+        const response = await axios.get("https://www.thecocktaildb.com/api/json/v1/1/search.php?s=autodafe");
+        const result = response.data.drinks;
+
+        res.render("featured.ejs", { recipe: result });
+    }
+    catch(error){
+        console.error(`Error: ${error.message}`);
+    }
+});
+
+app.get("/gin", async (req, res) => {
+    try {
+        const response = await axios.get("https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=178365");
+        const result = response.data.drinks;
+
+        res.render("featured.ejs", { recipe: result });
     }
     catch(error){
         console.error(`Error: ${error.message}`);
